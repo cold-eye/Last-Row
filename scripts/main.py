39119b5c7d16d055cc2d569a1081663ec21a688a
saeru.yamamuro01 <saeru.yamamuro01@g.softbank.co.jp>
2020-08-18 15:54:02 +0900

    [add] image, page title

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,13 @@
 import os
+import requests
+
+import streamlit as st
+
+from PIL import Image
 
 from utils import parse_args
 from views import set_sidebar
 from models import LiverpoolAnalyzer
-
 
 def main():
     # parse argument
@@ -17,6 +21,25 @@ def main():
         )
     else:
         exit(9)
+
+    # get image
+    image_path = os.path.join(base_dir, "reports", "figure", "profile.JPG")
+    md_path = os.path.join(base_dir, "scripts", "PROFILE.md")
+    if args.env == "local":
+        image = Image.open(image_path)
+        with open(md_path, "r") as fi:
+            profile_md = fi.read()
+    else:
+        profile_md = requests.get(md_path).content.decode(encoding="utf-8")
+        image = requests.get(image_path).content
+
+    # set layout
+    st.beta_set_page_config(
+        page_title="LiverpoolAnalyzer",
+        page_icon=image,
+        # layout="wide",
+        initial_sidebar_state="expanded"
+    )
 
     # set sidebar
     mode, play = set_sidebar(base_dir, args)
